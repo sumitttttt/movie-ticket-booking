58eb7ebcb7e79a7eaf5f65b97758733a20f542f8
Drake <sumitdrak@gmail.com>
2023-11-25 23:22:25 +0530

    more files changed related to the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
   return (
     <div>
-     <h1>MT-BW</h1>
+
+     {/* <h1>MT-BW</h1> */}
+
+    <BrowserRouter>
+    <Routes>  
+    <Route path="/" element={<Home/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/register" element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
+
     </div>
   );
 }
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-function Register() {
+function Login() {
   return (
-    <div>Register</div>
+    <div>Login</div>
   )
 }
 
-export default Register
+export default Login

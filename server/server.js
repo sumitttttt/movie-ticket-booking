7d45dd216f5d4a9a9afe9b0d7fadfc289c73d62d
Drake <sumitdrak@gmail.com>
2023-11-28 09:42:33 +0530

    .

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express();
-require('dotenv').config();
-const dbConfig = require('./config/dbConfig');
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, ()=> console.log(`server is running on ${port}`));
